f6ce2edad26ae92bdc5214c88386d24fc9a4d82c
Rodrigo nantes <rodrigosnantes@hotmail.com>
2024-11-20 18:57:50 -0400

    create alert dialog component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Select } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 import { Tooltip } from '@/components/ui/tooltip';
+import { AlertDialog } from '@/components/ui/alert-dialog';
 import { useForm } from 'react-hook-form';
-
+import { Button } from '@/components/ui/button';
 import { Switch } from '@/components/ui/switch';
 
 export default function App() {
@@ -19,37 +20,47 @@ export default function App() {
   };
 
   return (
-    <div className="w-full min-h-screen flex items-center justify-center">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        defaultValue={{
-          switch: true,
-        }}
-      >
-        <div>
-          {/* <Select
-            control={control}
-            name="select"
-            rules={{ required: 'Campo obrigatório' }}
-            placeholder="Selecione uma fruta"
-            options={[
-              { name: 'Manga', value: 'Manga' },
-              { name: 'Melancia', value: 'Melancia' },
-            ]}
-          />
-
-          <Switch name="switch" control={control} label="Rodrigo" /> */}
-        </div>
-
-        <div className="mt-5 flex gap-4 justify-end">
-          <button type="submit" className="border-2 p-1">
-            Enviar
-          </button>
-          <button type="button" onClick={() => onReset()}>
-            limpar
-          </button>
-        </div>
-      </form>
-    </div>
+    <>
+      <AlertDialog
+        title="Rodrigo"
+        subtitle="Algo aqui"
+        elementTrigger={<Button variant="outline">tezte</Button>}
+      />
+    </>
   );
+
+  // return (
+  //   <div className="w-full min-h-screen flex items-center justify-center">
+  //     <form
+  //       onSubmit={handleSubmit(onSubmit)}
+  //       defaultValue={{
+  //         switch: true,
+  //       }}
+  //     >
+  //       <div>
+  //         {/* <Select
+  //           control={control}
+  //           name="select"
+  //           rules={{ required: 'Campo obrigatório' }}
+  //           placeholder="Selecione uma fruta"
+  //           options={[
+  //             { name: 'Manga', value: 'Manga' },
+  //             { name: 'Melancia', value: 'Melancia' },
+  //           ]}
+  //         />
+
+  //         <Switch name="switch" control={control} label="Rodrigo" /> */}
+  //       </div>
+
+  //       <div className="mt-5 flex gap-4 justify-end">
+  //         <button type="submit" className="border-2 p-1">
+  //           Enviar
+  //         </button>
+  //         <button type="button" onClick={() => onReset()}>
+  //           limpar
+  //         </button>
+  //       </div>
+  //     </form>
+  //   </div>
+  // );
 }
---
+++ src/components/ui/alert-dialog.tsx
@@ -0,0 +1,206 @@
+import * as React from 'react';
+import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog';
+import { Button, buttonVariants } from '@/components/ui/button';
+
+import { cn } from '@/lib/utils';
+
+/**
+ * {@link https://ui.shadcn.com/docs/components/alert-dialog}.
+ */
+
+const AlertDialogModel = AlertDialogPrimitive.Root;
+
+const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
+
+const AlertDialogPortal = AlertDialogPrimitive.Portal;
+
+const AlertDialogOverlay = React.forwardRef<
+  React.ElementRef<typeof AlertDialogPrimitive.Overlay>,
+  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Overlay>
+>(({ className, ...props }, ref) => (
+  <AlertDialogPrimitive.Overlay
+    className={cn(
+      'fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      className
+    )}
+    {...props}
+    ref={ref}
+  />
+));
+AlertDialogOverlay.displayName = AlertDialogPrimitive.Overlay.displayName;
+
+const AlertDialogContent = React.forwardRef<
+  React.ElementRef<typeof AlertDialogPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>
+>(({ className, ...props }, ref) => (
+  <AlertDialogPortal>
+    <AlertDialogOverlay />
+    <AlertDialogPrimitive.Content
+      ref={ref}
+      className={cn(
+        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
+        className
+      )}
+      {...props}
+    />
+  </AlertDialogPortal>
+));
+AlertDialogContent.displayName = AlertDialogPrimitive.Content.displayName;
+
+const AlertDialogHeader = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      'flex flex-col space-y-2 text-center sm:text-left',
+      className
+    )}
+    {...props}
+  />
+);
+AlertDialogHeader.displayName = 'AlertDialogHeader';
+
+const AlertDialogFooter = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
+      className
+    )}
+    {...props}
+  />
+);
+AlertDialogFooter.displayName = 'AlertDialogFooter';
+
+const AlertDialogTitle = React.forwardRef<
+  React.ElementRef<typeof AlertDialogPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <AlertDialogPrimitive.Title
+    ref={ref}
+    className={cn('text-lg font-semibold', className)}
+    {...props}
+  />
+));
+AlertDialogTitle.displayName = AlertDialogPrimitive.Title.displayName;
+
+const AlertDialogDescription = React.forwardRef<
+  React.ElementRef<typeof AlertDialogPrimitive.Description>,
+  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Description>
+>(({ className, ...props }, ref) => (
+  <AlertDialogPrimitive.Description
+    ref={ref}
+    className={cn('text-sm text-muted-foreground', className)}
+    {...props}
+  />
+));
+AlertDialogDescription.displayName =
+  AlertDialogPrimitive.Description.displayName;
+
+const AlertDialogAction = React.forwardRef<
+  React.ElementRef<typeof AlertDialogPrimitive.Action>,
+  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action>
+>(({ className, ...props }, ref) => (
+  <AlertDialogPrimitive.Action
+    ref={ref}
+    className={cn(buttonVariants(), className)}
+    {...props}
+  />
+));
+AlertDialogAction.displayName = AlertDialogPrimitive.Action.displayName;
+
+const AlertDialogCancel = React.forwardRef<
+  React.ElementRef<typeof AlertDialogPrimitive.Cancel>,
+  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Cancel>
+>(({ className, ...props }, ref) => (
+  <AlertDialogPrimitive.Cancel
+    ref={ref}
+    className={cn(
+      buttonVariants({ variant: 'outline' }),
+      'mt-2 sm:mt-0',
+      className
+    )}
+    {...props}
+  />
+));
+AlertDialogCancel.displayName = AlertDialogPrimitive.Cancel.displayName;
+
+const AlertDialog = React.forwardRef<
+  React.ElementRef<typeof AlertDialogPrimitive.Cancel>,
+  AlertProps
+>(
+  (
+    {
+      className,
+      title,
+      subtitle,
+      elementTrigger,
+      cancelSettings,
+      confirmSettings,
+    },
+    ref
+  ) => {
+    const handleCancel = React.useCallback(() => {
+      cancelSettings.callback?.();
+    }, [cancelSettings]);
+
+    const handleConfirm = React.useCallback(() => {
+      confirmSettings.callback?.();
+    }, [confirmSettings]);
+
+    return (
+      <AlertDialogModel>
+        <AlertDialogTrigger asChild>{elementTrigger}</AlertDialogTrigger>
+
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{title}</AlertDialogTitle>
+            <AlertDialogDescription>{subtitle}</AlertDialogDescription>
+          </AlertDialogHeader>
+
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={handleCancel}>
+              {cancelSettings.label || 'Cancel'}
+            </AlertDialogCancel>
+            <AlertDialogAction onClick={handleConfirm}>
+              {confirmSettings.label || 'Continue'}
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogModel>
+    );
+  }
+);
+
+export type AlertProps = {
+  className?: string;
+  title?: string;
+  subtitle?: string;
+  elementTrigger: any;
+  cancelSettings: settings;
+  confirmSettings: settings;
+};
+
+export type settings = {
+  label: string;
+  callback?: any;
+  className?: string;
+};
+
+export {
+  AlertDialog,
+  AlertDialogModel,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+};
